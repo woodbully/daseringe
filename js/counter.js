aecacda55c035596cd5aaac534b732cb911fdb6b
Idakronborgn <55064627+Idakronborgn@users.noreply.github.com>
2020-12-08 13:04:01 +0100

    pregress counter

---
+++ js/counter.js
@@ -0,0 +1,27 @@
+
+// donation tæller
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach(counter => {
+counter.innerText = '500';
+
+const updateCounter = () => {
+const target = +counter.getAttribute('data-target');
+
+const c = +counter.innerText;
+
+const increment = target / 1000;
+
+if(c < target) {
+    counter.innerText = c + increment;
+    setTimeout(updateCounter, 1);
+
+} else {
+    counter.innerText = target;
+}
+
+};
+
+updateCounter();
+
+});
--- js/main.js
+++ js/main.js
@@ -37,3 +37,13 @@ function getFeaturedImageUrl(post) {
   }
   return imageUrl;
 }
+
+
+
+// donation tæller
+$('.counter').counterUp({
+  delay: 10,
+  time: 2000
+});
+$('.counter').addClass('animated fadeInDownBig');
+$('h3').addClass('animated fadeIn');
